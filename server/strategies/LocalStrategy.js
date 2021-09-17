24a71a8f6dc26d95296a5519b00ce89f7467d9cd
skander ben alaya <benalayaskander88@gmail.com>
2021-09-17 15:09:20 +0100

    new folder structure

--- server/strategies/LocalStrategy.js
+++
@@ -1,7 +0,0 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const user = require("../models/user.model");
-//Called during login/sign up.
-passport.use(new LocalStrategy(user.authenticate()));
-//called while after logging in / signing up to set user details in req.user
-passport.serializeUser(user.serializeUser());
--- src/js/utils/px2vh.js
+++
@@ -1,2 +0,0 @@
-const px2vh = (size, height = 900) => `${(size / height) * 100}vh`;
-export default px2vh;
